e9042e924d8a1e12ade419790efd7492e4fe4b27
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-03-15 16:41:36 +0100

    Style the app

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Task } from '../../task';
-
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { NgStyle } from '@angular/common';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [FaIconComponent, NgStyle],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent implements OnInit {
   @Input() task!: Task;
+  faTimes = faTimes;
   constructor() {}
   ngOnInit(): void {}
 }
